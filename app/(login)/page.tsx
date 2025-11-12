c044cbf5edea30d62652a8c7b1503175b3bb79b1
adnansyed101 <sirgeant10@gmail.com>
2025-11-12 14:53:08 +0600

    Remove layout component.

--- app/(login)/page.tsx
+++ app/(login)/page.tsx
@@ -1,27 +1,24 @@
 import LoginForm from "./components/LoginForm";
 import Link from "next/link";
 import Image from "next/image";
-import MainLayout from "../provider/MainLayout";
 
 export default function LoginPage() {
   return (
-    <MainLayout>
-      <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-        <div className="flex w-full max-w-sm flex-col gap-6">
-          <Link
-            href="/"
-            className="flex items-center gap-2 self-center font-medium w-60"
-          >
-            <Image
-              src="/arbree_logo_full_black.png"
-              alt="Arbree Solutions Logo"
-              width={200}
-              height={100}
-            />
-          </Link>
-          <LoginForm />
-        </div>
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <Link
+          href="/"
+          className="flex items-center gap-2 self-center font-medium w-60"
+        >
+          <Image
+            src="/arbree_logo_full_black.png"
+            alt="Arbree Solutions Logo"
+            width={200}
+            height={100}
+          />
+        </Link>
+        <LoginForm />
       </div>
-    </MainLayout>
+    </div>
   );
 }
