d3f375509bbf685a8f9ca85c3995261e6c7549f9
Md Adnan <sirgeant10@gmail.com>
2025-11-13 22:51:59 +0600

    Add links and rename layout.

---
+++ app/hr-management/layout.tsx
@@ -0,0 +1,40 @@
+import { Home, Logs, User } from "lucide-react";
+import { Links } from "../lib/types/types";
+import MainLayout from "../provider/MainLayout";
+
+export default function HRManagementLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const links: Links = [
+    {
+      label: "Essential Links",
+      items: [
+        {
+          title: "Home",
+          url: "/home",
+          icon: Home,
+        },
+        {
+          title: "Dashboard",
+          url: "/hr-management/dashboard",
+          icon: Logs,
+        },
+        {
+          title: "Personal Information",
+          url: "/hr-management/personal-information",
+          icon: User,
+        },
+      ],
+    },
+  ];
+
+  return (
+    <html>
+      <body>
+        <MainLayout links={links}>{children}</MainLayout>
+      </body>
+    </html>
+  );
+}
