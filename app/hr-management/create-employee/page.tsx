6dc2b252d9421d9ff24313f7a8124a08fb4efa0b
adnansyed101 <sirgeant10@gmail.com>
2025-11-13 22:17:53 +0600

    Add page elements.

--- app/hr-management/create-employee/page.tsx
+++ app/hr-management/create-employee/page.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import CreateEmployeeForm from "./component/create-employee-form";
 
 const CreateEmployeePage = () => {
-  return <div>CreateEmployeePage</div>;
+  return (
+    <main>
+      <div>
+        <h1 className="text-4xl font-bold text-foreground mb-4">
+          Create Employee
+        </h1>
+        <div>
+          <CreateEmployeeForm />
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default CreateEmployeePage;
