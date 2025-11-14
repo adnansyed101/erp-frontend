80079178203b084528ecb1dd66844a4f862cbf6a
adnansyed101 <sirgeant10@gmail.com>
2025-11-14 16:25:53 +0600

    Create steper for create employee.

---
+++ app/hr-management/create-employee/component/create-employee-steps.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+
+const CreateEmployeeSteps = ({ current = 0 }) => {
+  const steps = [
+    {
+      id: 1,
+      url: "/hr-management/create-employee/personal-information",
+      title: "Personal Information",
+    },
+    {
+      id: 2,
+      url: "/hr-management/create-employee/permanent-address",
+      title: "Permanent Address",
+    },
+    {
+      id: 3,
+      url: "/hr-management/create-employee/present-address",
+      title: "Present Address",
+    },
+    {
+      id: 4,
+      url: "/hr-management/create-employee/spouse-information",
+      title: "Spouse Information",
+    },
+    {
+      id: 5,
+      url: "/hr-management/create-employee/bank-information",
+      title: "Bank Information",
+    },
+  ];
+
+  return (
+    <div className="flex flex-col space-x-2 space-y-2 mb-4">
+      {steps.map((step, index) => (
+        <div key={step.id} className="border-l border-gray-400 p-2">
+          <Link
+            href={step.url}
+            className={cn(
+              "p-2 w-56 rounded-full text-center text-sm",
+              index === current ? "bg-secondary" : ""
+            )}
+          >
+            {step.title}
+          </Link>
+          
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default CreateEmployeeSteps;
