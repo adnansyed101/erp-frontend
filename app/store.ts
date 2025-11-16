2e504063ac1a0d5b2593fdbbbfb1d59e576f8fee
adnansyed101 <sirgeant10@gmail.com>
2025-11-16 19:01:45 +0600

    Persist employee data in store.

--- app/store.ts
+++ app/store.ts
@@ -1,10 +1,14 @@
 import { create } from "zustand";
 import { Employee } from "@/lib/types/types";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 type EmployeeDataState = Partial<Employee> & {
   setData: (data: Partial<Employee>) => void;
 };
 
-export const useEmployeeDataStore = create<EmployeeDataState>((set) => ({
-  setData: (data) => set(data),
-}));
+export const useEmployeeDataStore = create<EmployeeDataState>()(
+  persist((set) => ({ setData: (data) => set(data) }), {
+    name: "employee-data-storage",
+    storage: createJSONStorage(()=> localStorage)
+  }),
+);
