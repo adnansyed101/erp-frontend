652f390567cb8c154bb0ac0e31bb25507984f116
adnansyed101 <sirgeant10@gmail.com>
2025-11-18 00:30:28 +0600

    Fetch data from server adn show to client.

--- app/hr-management/employee-list/page.tsx
+++ app/hr-management/employee-list/page.tsx
@@ -1,9 +1,62 @@
-import React from 'react'
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import { Employee } from "@/lib/types/types";
+import { EmployeeCard } from "./components/employee-card";
 
 const EmployeeListPage = () => {
-  return (
-    <div>EmployeeListPage</div>
-  )
-}
+  const {
+    data: employees,
+    isPending,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["employees"],
+    queryFn: async (): Promise<{
+      success: boolean;
+      message: string;
+      data: Employee[];
+    }> => {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_SERVER_URL}/hr-management/all-employees`
+      );
 
-export default EmployeeListPage
+      if (!response.ok) {
+        throw new Error("Failed to fetch all employee");
+      }
+
+      return response.json();
+    },
+    // initialData: { success: false, message: "fetching data", data: [] },
+  });
+
+  console.log(employees);
+
+  if (error) return "An error has occurred: " + error.message;
+
+  return (
+    <main>
+      <h1 className="text-4xl font-bold text-foreground mb-4">Employee List</h1>
+      {isLoading || isPending ? (
+        "Loading...."
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+          {employees.data.map((employee) => (
+            <EmployeeCard
+              key={employee.id}
+              id={employee.id || "1"}
+              imgUrl={employee.imageUrl}
+              fullName={employee.fullName}
+              officeEmail={employee.officeEmail}
+              personalNumber={employee.personalNumber}
+              currentDesignation={employee.currentDesignation}
+              jobTitle={employee.branchName}
+            />
+          ))}
+        </div>
+      )}
+    </main>
+  );
+};
+
+export default EmployeeListPage;
