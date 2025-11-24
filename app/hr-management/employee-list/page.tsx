8c55ee1dba48e8e73957ed0faab682c8bef064ab
adnansyed101 <sirgeant10@gmail.com>
2025-11-24 15:33:06 +0600

    Convert page to server side rendering.

--- app/hr-management/employee-list/page.tsx
+++ app/hr-management/employee-list/page.tsx
@@ -1,58 +1,27 @@
-"use client";
-
-import { useQuery } from "@tanstack/react-query";
-import { Employee } from "@/lib/types/types";
 import { EmployeeCard } from "./components/employee-card";
+import { getEmployees } from "@/lib/actions/employee.action";
 
-const EmployeeListPage = () => {
-  const {
-    data: employees,
-    isPending,
-    isLoading,
-    error,
-  } = useQuery({
-    queryKey: ["employees"],
-    queryFn: async (): Promise<{
-      success: boolean;
-      message: string;
-      data: Employee[];
-    }> => {
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_SERVER_URL}/hr-management/all-employees`
-      );
-
-      if (!response.ok) {
-        throw new Error("Failed to fetch all employee");
-      }
-
-      return response.json();
-    },
-  });
-
-  if (error) return "An error has occurred: " + error.message;
+const EmployeeListPage = async () => {
+  const employees = await getEmployees();
 
   return (
     <main>
       <h1 className="text-4xl font-bold text-foreground mb-4">Employee List</h1>
-      {isLoading || isPending ? (
-        "Loading...."
-      ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-          {employees.data.length === 0 && <p>No Employees Registered</p>}
-          {employees.data.map((employee) => (
-            <EmployeeCard
-              key={employee.id}
-              id={employee.id || "1"}
-              imgUrl={employee.imageUrl}
-              fullName={employee.fullName}
-              officeEmail={employee.officeEmail}
-              personalNumber={employee.personalNumber}
-              currentDesignation={employee.currentDesignation}
-              jobTitle={employee.branchName}
-            />
-          ))}
-        </div>
-      )}
+      {employees.length === 0 && <p>No Employees Registered</p>}
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+        {employees.map((employee) => (
+          <EmployeeCard
+            key={employee.id}
+            id={employee.id || "1"}
+            imgUrl={employee.imageUrl}
+            fullName={employee.fullName}
+            officeEmail={employee.officeEmail}
+            personalNumber={employee.personalNumber}
+            currentDesignation={employee.currentDesignation}
+            jobTitle={employee.branchName}
+          />
+        ))}
+      </div>
     </main>
   );
 };
