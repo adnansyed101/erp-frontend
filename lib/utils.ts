00ddd75c2fb339125d72b4d10af7b7b221b16c28
adnansyed101 <sirgeant10@gmail.com>
2025-11-25 16:21:25 +0600

    Create utility functions.

--- lib/utils.ts
+++ lib/utils.ts
@@ -9,3 +9,28 @@ export function cn(...inputs: ClassValue[]) {
 export function convertToPlainObject<T>(value: T): T {
   return JSON.parse(JSON.stringify(value));
 }
+
+// Format error
+export function formatError(error: any) {
+  if (error.name === "ZodError") {
+    // Handle Zod Error
+    const fieldErrors = Object.keys(error.errors).map(
+      (field) => error.errors[field].message
+    );
+
+    return fieldErrors.join(". ");
+  } else if (
+    error.name === "PrismaClientKnownRequestError" &&
+    error.code === "P2002"
+  ) {
+    // Handle Prisma Error
+    const field = error.meta?.target ? error.meta.target[0] : "Field";
+
+    return `${field.charAt(0).toUpperCase() + field.slice(1)} already exists.`;
+  } else {
+    // Handle other errors
+    return typeof error.message === "string"
+      ? error.message
+      : JSON.stringify(error.message);
+  }
+}
