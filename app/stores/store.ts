05d7e448d570780f68a7cc299095d6fc1b150ea4
adnansyed101 <sirgeant10@gmail.com>
2025-11-25 16:18:47 +0600

    Move to store folder.

---
+++ app/stores/store.ts
@@ -0,0 +1,19 @@
+import { create } from "zustand";
+import { Employee } from "@/lib/types/types";
+import { persist, createJSONStorage } from "zustand/middleware";
+
+export type EmployeeDataState = Partial<Employee> & {
+  setData: (data: Partial<Employee>) => void;
+};
+
+export const useEmployeeDataStore = create<EmployeeDataState>()(
+  persist(
+    (set) => ({
+      setData: (data) => set(data),
+    }),
+    {
+      name: "employee-data-storage",
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);
