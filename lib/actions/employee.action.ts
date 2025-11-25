38b95decd26d51e174b864c5b873f2f0bf3f3407
adnansyed101 <sirgeant10@gmail.com>
2025-11-25 16:19:08 +0600

    Create action to insert employee to database.

--- lib/actions/employee.action.ts
+++ lib/actions/employee.action.ts
@@ -1,10 +1,52 @@
 "use server";
 
+import z from "zod";
 import { prisma } from "../prisma";
-import { convertToPlainObject } from "../utils";
+import { convertToPlainObject, formatError } from "../utils";
+import { EmployeeSchema } from "../validators/employee.validator";
 
+// Get all employees.
 export async function getEmployees() {
   const data = await prisma.employee.findMany({});
 
   return convertToPlainObject(data);
+}
+
+// Create new employee
+export async function createEmployee(data: z.infer<typeof EmployeeSchema>) {
+  try {
+    const newEmployee = await prisma.employee.create({
+      data: {
+        ...data,
+        presentAddress: {
+          create: {
+            ...data.presentAddress,
+          },
+        },
+        permanentAddress: {
+          create: {
+            ...data.permanentAddress,
+          },
+        },
+        spouseInformation: {
+          create: {
+            ...data.spouseInformation,
+          },
+        },
+        emergencyContact: {
+          create: {
+            ...data.emergencyContact,
+          },
+        },
+        additionalInformation: {
+          create: {
+            ...data.additionalInformation,
+          },
+        },
+      },
+    });
+    return convertToPlainObject(newEmployee);
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
 }
