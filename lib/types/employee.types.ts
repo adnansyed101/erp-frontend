1ea8ebf90431533610414e11503dadb31dbd217d
adnansyed101 <sirgeant10@gmail.com>
2025-11-26 12:09:05 +0600

    Create employee types.

---
+++ lib/types/employee.types.ts
@@ -0,0 +1,23 @@
+import z from "zod";
+import {
+  AdditionalInformationSchema,
+  BankInformationSchema,
+  EmergencyContactSchema,
+  EmployeeSchema,
+  PermanentAddressSchema,
+  PersonalInformationSchema,
+  PresentAddressSchema,
+  SpouseInformationSchema,
+} from "../validators/employee.validator";
+
+export type PersonalInformation = z.infer<typeof PersonalInformationSchema>;
+export type AddtionalInformation = z.infer<typeof AdditionalInformationSchema>;
+export type BankInformation = z.infer<typeof BankInformationSchema>;
+
+export type PresentAddress = z.infer<typeof PresentAddressSchema>;
+export type PermanentAddress = z.infer<typeof PermanentAddressSchema>;
+
+export type SpouseInformation = z.infer<typeof SpouseInformationSchema>;
+export type EmergencyContact = z.infer<typeof EmergencyContactSchema>;
+
+export type Employee = z.infer<typeof EmployeeSchema>;
