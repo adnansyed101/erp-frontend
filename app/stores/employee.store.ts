28e4bb99c5a90b854e72a5cb97ab4a093595198a
adnansyed101 <sirgeant10@gmail.com>
2025-11-30 16:58:46 +0600

    Update stores.

--- app/stores/employee.store.ts
+++ app/stores/employee.store.ts
@@ -10,15 +10,34 @@ import type {
   BankInformation,
 } from "@/lib/types/employee.types";
 
-type EmployeePersonalInformationState = Partial<PersonalInformation> & {
-  setData: (data: Partial<PersonalInformation>) => void;
+type EmployeePersonalInformationState = {
+  personalInformation: PersonalInformation;
+  setData: (data: PersonalInformation) => void;
 };
 
 export const useEmployeePersonalInformationDataStore =
   create<EmployeePersonalInformationState>()(
     persist(
       (set) => ({
-        setData: (data) => set(data),
+        personalInformation: {
+          fullName: "",
+          imageUrl: "",
+          officeEmail: "",
+          personalEmail: "",
+          personalNumber: "",
+          officeNumber: "",
+          employeeType: "",
+          employeeStatus: "",
+          nationality: "",
+          disability: false,
+          gender: "Male",
+          religion: "",
+          joiningDesignation: "",
+          currentDesignation: "",
+          dateOfBirth: new Date(),
+          dateOfConfirmation: new Date(),
+        },
+        setData: (data) => set({ personalInformation: data }),
       }),
       {
         name: "employee-personal-information-data-storage",
@@ -27,69 +46,163 @@ export const useEmployeePersonalInformationDataStore =
     )
   );
 
-type AdditionalInformationState = Partial<AddtionalInformation> & {
-  setData: (data: Partial<AddtionalInformation>) => void;
+type AdditionalInformationState = {
+  addtionalInformation: AddtionalInformation;
+  setData: (data: AddtionalInformation) => void;
 };
 
 export const useEmployeeAdditionalInformation =
   create<AdditionalInformationState>()(
-    persist((set) => ({ setData: (data) => set(data) }), {
-      name: "employee-additional-information-data-storage",
-      storage: createJSONStorage(() => localStorage),
-    })
+    persist(
+      (set) => ({
+        addtionalInformation: {
+          fatherName: "",
+          motherName: "",
+          nationalId: "",
+          placeOfBirth: "",
+          maritalStatus: "Single",
+          eTIN: "",
+          program: "",
+          unit: "",
+          prlDate: new Date(),
+          dateofRegularity: new Date(),
+        },
+        setData: (data) => set({ addtionalInformation: data }),
+      }),
+      {
+        name: "employee-additional-information-data-storage",
+        storage: createJSONStorage(() => localStorage),
+      }
+    )
   );
 
-type PresentAddressState = Partial<PresentAddress> & {
-  setData: (data: Partial<PresentAddress>) => void;
+type PresentAddressState = {
+  presentAddress: PresentAddress;
+  setData: (data: PresentAddress) => void;
 };
 
 export const useEmployeePresentAddress = create<PresentAddressState>()(
-  persist((set) => ({ setData: (data) => set(data) }), {
-    name: "employee-present-address-data-storage",
-    storage: createJSONStorage(() => localStorage),
-  })
+  persist(
+    (set) => ({
+      presentAddress: {
+        division: "",
+        district: "",
+        upazilaOrThana: "",
+        postOffice: "",
+        postCode: "",
+        houseNoOrVillage: "",
+        block: "",
+        roadNo: "",
+      },
+
+      setData: (data) => set({ presentAddress: data }),
+    }),
+    {
+      name: "employee-present-address-data-storage",
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
 );
 
-type PermanentAddressState = Partial<PermanentAddress> & {
-  setData: (data: Partial<PermanentAddress>) => void;
+type PermanentAddressState = {
+  permanentAddress: PermanentAddress;
+  setData: (data: PermanentAddress) => void;
 };
 
 export const useEmployeePermanentAddress = create<PermanentAddressState>()(
-  persist((set) => ({ setData: (data) => set(data) }), {
-    name: "employee-permanent-address-data-storage",
-    storage: createJSONStorage(() => localStorage),
-  })
+  persist(
+    (set) => ({
+      permanentAddress: {
+        division: "",
+        district: "",
+        upazilaOrThana: "",
+        postOffice: "",
+        postCode: "",
+        houseNoOrVillage: "",
+        block: "",
+        roadNo: "",
+      },
+      setData: (data) => set({ permanentAddress: data }),
+    }),
+    {
+      name: "employee-permanent-address-data-storage",
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
 );
 
-type SpouseInformationState = Partial<SpouseInformation> & {
-  setData: (data: Partial<SpouseInformation>) => void;
+type SpouseInformationState = {
+  spouseInformation: SpouseInformation;
+  setData: (data: SpouseInformation) => void;
 };
 
 export const useEmployeeSpouseInformation = create<SpouseInformationState>()(
-  persist((set) => ({ setData: (data) => set(data) }), {
-    name: "employee-permanent-address-data-storage",
-    storage: createJSONStorage(() => localStorage),
-  })
+  persist(
+    (set) => ({
+      spouseInformation: {
+        fullName: "",
+        dateOfBirth: new Date(),
+        gender: "Male",
+        occupation: "",
+        nid: "",
+        mobileNumber: "",
+        email: "",
+      },
+      setData: (data) => set({ spouseInformation: data }),
+    }),
+    {
+      name: "employee-spouse-information-data-storage",
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
 );
 
-type EmergencyContactState = Partial<EmergencyContact> & {
-  setData: (data: Partial<EmergencyContact>) => void;
+type EmergencyContactState = {
+  emergencyContact: EmergencyContact;
+  setData: (data: EmergencyContact) => void;
 };
 
 export const useEmployeeEmergencyContact = create<EmergencyContactState>()(
-  persist((set) => ({ setData: (data) => set(data) }), {
-    name: "employee-permanent-address-data-storage",
-    storage: createJSONStorage(() => localStorage),
-  })
+  persist(
+    (set) => ({
+      emergencyContact: {
+        fullName: "",
+        dateOfBirth: new Date(),
+        gender: "Male",
+        occupation: "",
+        nid: "",
+        mobileNumber: "",
+        email: "",
+      },
+      setData: (data) => set({ emergencyContact: data }),
+    }),
+    {
+      name: "employee-emrgency-contact-data-storage",
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
 );
 
-type BankInformationState = Partial<BankInformation> & {
-  setData: (data: Partial<BankInformation>) => void;
+type BankInformationState = {
+  bankInformation: BankInformation;
+  setData: (data: BankInformation) => void;
 };
 
 export const useBankInformationState = create<BankInformationState>()(
-  persist((set) => ({ setData: (data) => set(data) }), {
-    name: "employee-bank-information-data-storage",
-    storage: createJSONStorage(() => localStorage),
-  })
+  persist(
+    (set) => ({
+      bankInformation: {
+        bankName: "",
+        branchName: "",
+        accountNumber: "",
+        walletType: "",
+        walletNumber: "",
+      },
+      setData: (data) => set({ bankInformation: data }),
+    }),
+    {
+      name: "employee-bank-information-data-storage",
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
 );
