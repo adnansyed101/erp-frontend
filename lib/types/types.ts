49f454dd1821dc8413d4dd1abd11de2c8924f00b
adnansyed101 <sirgeant10@gmail.com>
2025-12-03 17:58:00 +0600

    Remove additional type.

--- lib/types/types.ts
+++ lib/types/types.ts
@@ -1,6 +1,4 @@
 import { LucideIcon } from "lucide-react";
-import z from "zod";
-import { EmployeeSchema } from "../validators/employee.validator";
 
 export type Links = {
   label: string;
@@ -11,5 +9,3 @@ export type Links = {
     isActive?: boolean;
   }[];
 }[];
-
-export type Employee = z.infer<typeof EmployeeSchema>;
