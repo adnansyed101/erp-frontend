db8bcceaf552f61381234e432d7b0fb66eb2e0cc
adnansyed101 <sirgeant10@gmail.com>
2025-12-08 22:50:55 +0600

    Create schema.

--- lib/validators/attendance.schema.ts
+++ lib/validators/attendance.schema.ts
@@ -1,8 +1,15 @@
 import z from "zod";
+import { EmployeeSchema } from "./employee.validator";
 
 export const AttendanceSchema = z.object({
   checkIn: z.date().default(new Date()),
-  checkOut: z.date().nullable(),
+  checkOut: z.date().optional(),
+  preferableInTime: z.date(),
   status: z.enum(["In", "Out"]),
   employeeId: z.string(),
+});
+
+export const AttendanceSchemaWithEmployeeData = AttendanceSchema.extend({
+  id: z.string(),
+  employee: EmployeeSchema,
 });
