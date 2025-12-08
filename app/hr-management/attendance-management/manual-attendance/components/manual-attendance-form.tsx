f21e899929687bff6fb7f3d88eeb5e15c05fad89
adnansyed101 <sirgeant10@gmail.com>
2025-12-08 12:40:45 +0600

    Add date picker to manual attendance form.

--- app/hr-management/attendance-management/manual-attendance/components/manual-attendance-form.tsx
+++ app/hr-management/attendance-management/manual-attendance/components/manual-attendance-form.tsx
@@ -1,31 +1,68 @@
 "use client";
 
-import type React from "react";
-
-import { useState } from "react";
-import { Button } from "@/components/ui/button";
+import React from "react";
+import { FormEvent, useState } from "react";
 import { Input } from "@/components/ui/input";
+import { LogIn, LogOut, Clock, ChevronDownIcon } from "lucide-react";
+import { SearchDropdown } from "./search-dropdown";
+import { useQuery } from "@tanstack/react-query";
+import { EmployeeWithId } from "@/lib/types/employee.types";
+import { format, formatDate } from "date-fns";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
 import { Label } from "@/components/ui/label";
-import { LogIn, LogOut, Clock } from "lucide-react";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 
 interface AttendanceFormData {
-  employeeName: string;
-  designation: string;
-  department: string;
-  loginDate: string;
+  loginDate: Date;
   loginTime: string;
   weekDay: string;
   preferableInTime: string;
 }
 
 export function ManualAttendanceForm() {
+  const {
+    data: employees,
+    isPending,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["employees"],
+    queryFn: async (): Promise<{
+      success: boolean;
+      message: string;
+      data: EmployeeWithId[];
+    }> => {
+      const response = await fetch(`/api/hr-management/employees`);
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch all employee");
+      }
+
+      return response.json();
+    },
+    initialData: { success: false, message: "Fetching Data", data: [] },
+  });
+  const [open, setOpen] = React.useState(false);
+
+  const weekday = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
   const [formData, setFormData] = useState<AttendanceFormData>({
-    employeeName: "",
-    designation: "",
-    department: "",
-    loginDate: "",
+    loginDate: new Date(),
     loginTime: "",
-    weekDay: "",
+    weekDay: weekday[new Date().getDay()],
     preferableInTime: "",
   });
 
@@ -37,12 +74,22 @@ export function ManualAttendanceForm() {
     }));
   };
 
-  const handleSave = () => {
+  const handleSave = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
     console.log("Form data saved:", formData);
   };
 
+  const handleSearch = (item: any) => {
+    console.log(item);
+  };
+
+  const handleSelect = (value: EmployeeWithId) => {
+    console.log(value);
+  };
+
   return (
-    <form className="space-y-6">
+    <form className="space-y-6" onSubmit={(e) => handleSave(e)}>
       {/* Action Buttons */}
       <div className="flex gap-2">
         <Button
@@ -70,122 +117,107 @@ export function ManualAttendanceForm() {
       </div>
 
       {/* Form Fields */}
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 py-6">
-        {/* Left Column */}
-        <div className="space-y-4">
+      <div className="space-y-4 py-6">
+        <div>
+          <Label htmlFor="employeeName" className="text-sm font-medium mb-1">
+            Emp Name <span className="text-red-500">*</span>
+          </Label>
+          <SearchDropdown
+            placeholder="Enter Employee Name"
+            items={employees.data}
+            onSearch={handleSearch}
+            onSelect={handleSelect}
+          />
+        </div>
+
+        <div className="grid grid-cols-2 gap-2">
           <div>
-            <Label htmlFor="employeeName" className="text-sm font-medium">
-              Emp Name <span className="text-red-500">*</span>
+            <Label htmlFor="loginDate" className="text-sm font-medium">
+              Login Date<span className="text-red-500">*</span>
             </Label>
-            <Input
-              id="employeeName"
-              name="employeeName"
-              placeholder="Employee Name"
-              value={formData.employeeName}
-              onChange={handleInputChange}
-              className="mt-1"
-            />
+            <Popover open={open} onOpenChange={setOpen}>
+              <PopoverTrigger asChild>
+                <Button
+                  variant="outline"
+                  id="date"
+                  className="w-full justify-between"
+                >
+                  {formData.loginDate
+                    ? formData.loginDate.toLocaleDateString()
+                    : "Select date"}
+                  <ChevronDownIcon />
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent
+                className="w-auto overflow-hidden p-0"
+                align="start"
+              >
+                <Calendar
+                  mode="single"
+                  selected={formData.loginDate}
+                  captionLayout="dropdown"
+                  onSelect={() => {
+                    setFormData((prev) => ({
+                      ...prev,
+                      loginDate: new Date(),
+                    }));
+                    setOpen(false);
+                  }}
+                />
+              </PopoverContent>
+            </Popover>
           </div>
 
           <div>
-            <Label htmlFor="designation" className="text-sm font-medium">
-              Designation
+            <Label htmlFor="weekDay" className="text-sm font-medium">
+              Week day
             </Label>
             <Input
-              id="designation"
-              name="designation"
-              placeholder="Designation"
-              value={formData.designation}
+              id="weekDay"
+              name="weekDay"
+              placeholder="Week day"
+              value={formData.weekDay}
               onChange={handleInputChange}
               className="mt-1"
-            />
-          </div>
-
-          <div>
-            <Label htmlFor="department" className="text-sm font-medium">
-              Department
-            </Label>
-            <Input
-              id="department"
-              name="department"
-              placeholder="Department"
-              value={formData.department}
-              onChange={handleInputChange}
-              className="mt-1"
+              disabled
             />
           </div>
         </div>
-
-        {/* Right Column */}
-        <div className="space-y-4">
-          <div className="grid grid-cols-2 gap-2">
-            <div>
-              <Label htmlFor="loginDate" className="text-sm font-medium">
-                Login Date<span className="text-red-500">*</span>
-              </Label>
-              <Input
-                id="loginDate"
-                name="loginDate"
-                type="date"
-                placeholder="Effective Date"
-                value={formData.loginDate}
-                onChange={handleInputChange}
-                className="mt-1"
-              />
-            </div>
-
-            <div>
-              <Label htmlFor="weekDay" className="text-sm font-medium">
-                Week day
-              </Label>
-              <Input
-                id="weekDay"
-                name="weekDay"
-                placeholder="Week day"
-                value={formData.weekDay}
-                onChange={handleInputChange}
-                className="mt-1"
-              />
-            </div>
-          </div>
-
-          <div>
-            <Label htmlFor="loginTime" className="text-sm font-medium">
-              Login Time<span className="text-red-500">*</span>
-            </Label>
-            <Input
-              id="loginTime"
-              name="loginTime"
-              type="time"
-              placeholder="In time"
-              value={formData.loginTime}
-              onChange={handleInputChange}
-              className="mt-1"
-            />
-          </div>
-
-          <div>
-            <Label htmlFor="preferableInTime" className="text-sm font-medium">
-              Preferable In Time
-            </Label>
-            <Input
-              id="preferableInTime"
-              name="preferableInTime"
-              type="time"
-              placeholder="Preferable In Time"
-              value={formData.preferableInTime}
-              onChange={handleInputChange}
-              className="mt-1"
-            />
-          </div>
+        <div>
+          <Label htmlFor="loginTime" className="text-sm font-medium">
+            Login Time<span className="text-red-500">*</span>
+          </Label>
+          <Input
+            id="loginTime"
+            name="loginTime"
+            type="time"
+            placeholder="In time"
+            value={formData.loginTime}
+            onChange={handleInputChange}
+            className="mt-1"
+          />
         </div>
+        {/* <div>
+          <Label htmlFor="preferableInTime" className="text-sm font-medium">
+            Preferable In Time
+          </Label>
+          <Input
+            id="preferableInTime"
+            name="preferableInTime"
+            type="time"
+            placeholder="Preferable In Time"
+            value={formData.preferableInTime}
+            onChange={handleInputChange}
+            className="mt-1"
+            
+          />
+        </div> */}
+        <Button>Current Time</Button>
       </div>
-
       {/* Save Button */}
       <div className="flex justify-end pt-4 border-t">
         <Button
-          type="button"
-          onClick={handleSave}
+          type="submit"
           className="bg-green-500 hover:bg-green-600 text-white gap-2"
         >
           <span>💾</span>
